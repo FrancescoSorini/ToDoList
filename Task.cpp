d40a6fdd31b086ed8824ff0c0672d907681f16c7
Francesco Sorini <sorini.francesco@gmail.com>
2023-03-21 18:02:57 +0100

    Creazione classe Task

---
+++ Task.cpp
@@ -0,0 +1,27 @@
+//
+// Created by sorin on 21/03/2023.
+//
+
+#include "Task.h"
+
+Task::Task() {}
+
+Task::Task(const std::string &title, bool completed = false) : title{title} {}
+
+const std::string &Task::getTitle() const {
+    return title;
+}
+
+void Task::setDescription(const std::string &description) {
+    Task::description = description;
+}
+
+bool Task::is_completed() {
+    return completed;
+}
+
+void Task::mark_completed() {
+    completed = true;
+}
+
+
---
+++ Task.h
@@ -0,0 +1,31 @@
+//
+// Created by sorin on 21/03/2023.
+//
+
+#ifndef TODOLIST_TASK_H
+#define TODOLIST_TASK_H
+
+#include <string>
+
+class Task {
+public:
+    Task();
+
+    explicit Task(const std::string &title, bool completed);
+
+    const std::string &getTitle() const;
+
+    void setDescription(const std::string &description);
+
+    bool is_completed();
+
+    void mark_completed();
+
+
+private:
+    std::string title;
+    std::string description;
+    bool completed;
+};
+
+#endif //TODOLIST_TASK_H
