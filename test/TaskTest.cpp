80ee6da972a6477717323b712147b44f361bc377
Francesco Sorini <sorini.francesco@gmail.com>
2023-04-10 22:38:15 +0200

    Aggiunto test operatore == di Task

--- test/TaskTest.cpp
+++ test/TaskTest.cpp
@@ -8,3 +8,14 @@ TEST(Task, DefaultConstructor) {
     Task t;
     ASSERT_FALSE(t.is_completed());
 }
+
+TEST(Task, EqualOperator) {
+    Date d(10, 4, 2023);
+    Date d1(1, 1, 2020);
+    Task t1("Task 1", "descrizione", d, false);
+    Task t2("Task 1", "descrizione", d, false);
+    Task t3("Task 3", "descr", d1, true);
+
+    ASSERT_TRUE(t1 == t2);
+    ASSERT_FALSE(t2 == t3);
+}
